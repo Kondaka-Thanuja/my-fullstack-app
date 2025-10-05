320db39008082cb6bad4c8315ed3e8ac5cefc195
Thanuja <kondakathanuja@gmail.com>
2025-10-05 19:31:45 +0530

    Initial commit: frontend + server

---
+++ server/server.js
@@ -0,0 +1,28 @@
+// server.js
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import authRoutes from "./routes/auth.js"; // Make sure this file exists
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
