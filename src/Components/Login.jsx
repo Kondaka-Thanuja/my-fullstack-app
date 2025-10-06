7cf8feb6dbbb03ac8cc708e4ed33a573f33f1737
Kondaka-Thanuja <kondakathanuja@gmail.com>
2025-10-06 22:15:07 +0530

    Update Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./Login.css";
 
-const API_URL = "https://backend-perplexity.onrender.com/";
+const API_URL = "https://backend-perplexity.onrender.com";
 
 
 
