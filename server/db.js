6ab1a72ef944cd7927326956b004e1b3eae51cd5
Thanuja <kondakathanuja@gmail.com>
2025-10-06 15:42:50 +0530

    Configured Railway MySQL connection

--- server/db.js
+++ server/db.js
@@ -3,15 +3,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD, // empty string is fine
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,    // mainline.proxy.rlwy.net
+  user: process.env.DB_USER,    // root
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME, 
+  port: process.env.DB_PORT     // 45729
 });
 
 db.connect((err) => {
   if (err) throw err;
-  console.log("✅ Connected to MySQL Database");
+  console.log("✅ Connected to Railway MySQL Database");
 });
 
 export default db;
